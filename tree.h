36fc2f6ee821776c475b99cdb2cc2c3811c94fe9
pierewoj <kuba.pierewoj@gmail.com>
2017-01-26 16:22:03 +0100

    init

--- tree.h
+++ tree.h
@@ -6,28 +6,87 @@
 #include <functional>
 #include <tuple>
 #include <limits>
+#include <queue>
+#include <cassert>
+
 using namespace std::placeholders;
 
 using Action = std::function<void()>;
-using Order = std::function<void(Action,Action,Action)>;
+using Order = std::function<void(Action, Action, Action)>;
 
-template <typename T>
+template<typename T>
 class Tree {
-public:
+private:
     struct Node;
-    using NodePtr = std::shared_ptr<Node const>;
-    struct Node {
-        Node (T const  value,  NodePtr const left, NodePtr const right) :
-                m_value(value), m_left(std::move(left)), m_right(std::move(right)) {}
-        Node() : m_value{ }, m_left{}, m_right{} {}
-        Node(T const value) : m_value(value) {}
-        T const  m_value;
-        NodePtr const m_left;
-        NodePtr const m_right;
-    };
+    using NodePtr = std::shared_ptr<Node>;
+    NodePtr m_root;
 
-    NodePtr const m_root;
+    Tree left() {
+        assert(!empty());
+        return Tree(m_root->m_left);
+    }
 
+    Tree right() {
+        assert(!empty());
+        return Tree(m_root->m_right);
+    }
+
+    T value() {
+        assert(!empty());
+        return m_root->get_value();
+    }
+
+    bool empty() {
+        return m_root == nullptr;
+    }
+
+    template<typename Functor>
+    Tree<T> filter_helper(Functor predicate, bool can_be_rightmost, std::queue<Tree> &toAppend) {
+        auto filter_left = [&]() -> Tree<T> { return left().filter_helper(predicate, false, toAppend); };
+        auto filter_right = [&]() -> Tree<T> { return right().filter_helper(predicate, can_be_rightmost, toAppend); };
+
+        if (empty()) {
+            if (toAppend.empty() || !can_be_rightmost) {
+                return Tree();
+            }
+            auto first = toAppend.front();
+            toAppend.pop();
+            return first.filter_helper(predicate, true, toAppend);
+        }
+
+        if (predicate(value())) {
+            auto left_filtered = filter_left();
+            auto right_filtered = filter_right();
+            return createValueNode(value(), left_filtered, right_filtered);
+        }
+
+        if (left().empty() && !right().empty()) {
+            return filter_right();
+        }
+
+        if (!left().empty() && right().empty()) {
+            return filter_left();
+        }
+
+        if (left().empty() && right().empty()) {
+            return Tree();
+        }
+
+        toAppend.push(right());
+        return left().filter_helper(predicate, can_be_rightmost, toAppend);
+    }
+
+    template<typename Functor>
+    void accumulate_helper(Functor operation, T &a, Order traversal) {
+        if (empty()) {
+            return;
+        }
+        auto nodeFun = [&] { a = operation(a, value()); };
+        auto leftFun = [&] { left().accumulate_helper(operation, a, traversal); };
+        auto rightFun = [&] { right().accumulate_helper(operation, a, traversal); };
+        traversal(nodeFun, leftFun, rightFun);
+    }
+public:
     Tree(NodePtr root) : m_root(std::move(root)) {}
 
     Tree() = default;
@@ -40,135 +99,163 @@ public:
 
     static Order inorder, preorder, postorder;
 
-    Tree left() const {
-        if (empty()) {
-            throw std::logic_error("Tree::left(): Empty BST");
-        }
-        return Tree(m_root->m_left);
-    }
-
-    Tree right() const {
-        if (empty()) {
-            throw std::logic_error("Tree:right() Empty BST");
-        }
-        return Tree(m_root->m_right);
-    }
-
-    T value() const {
-        if (empty()) {
-            throw std::logic_error("Tree:right() Empty BST");
-        }
-        return m_root->m_value;
-    }
-
-    bool empty() const {
-        return m_root == nullptr;
-    }
-
     static Tree createEmptyNode() {
         return Tree();
     }
 
-    static Tree createValueNode(T value) {
+    template<typename TVal>
+    static Tree createValueNode(TVal value) {
         auto ptr = std::make_shared<Node>(Node(std::move(value)));
         return Tree(ptr);
     }
 
-    static Tree createValueNode(T value, Tree left, Tree right) {
-        auto ptr = std::make_shared<Node>(Node(value, left.m_root, right.m_root));
+    template<typename TVal>
+    static Tree createValueNode(TVal val, Tree left, Tree right) {
+        auto ptr = std::make_shared<Node>(Node(val, left.m_root, right.m_root));
         return Tree(ptr);
     }
 
-    template<typename R>
-    R fold(std::function<R(T,R,R)> operation, R init) const {
-        if(empty())
-        {
+    template<typename R, typename Functor>
+    R fold(Functor operation, R init) {
+        if (empty()) {
             return init;
         }
-        auto value = m_root->m_value;
+        auto val = value();
         auto l_result = left().fold(operation, init);
         auto r_result = right().fold(operation, init);
-        return operation(value, l_result, r_result);
+        return operation(val, l_result, r_result);
     }
 
-    Tree<T> map(std::function<T(T)> transformer) const
-    {
-        if(empty())
+    template<typename Functor>
+    Tree<T> map(Functor transformer) {
+        if (empty())
             return Tree();
-        auto mapped_value = transformer(m_root->m_value);
+        auto mapped_value = transformer(value());
         return createValueNode(mapped_value, left().map(transformer), right().map(transformer));
     }
 
-    T accumulate(std::function<T(T,T)> operation, T a, Order traversal) const
-    {
+    template<typename Functor>
+    Tree<T> lazy_map(Functor transformer) {
+        if (empty())
+            return Tree();
+        auto transformed_val = value();
+        auto value_mapper = [=]() -> T { return transformer(transformed_val); };
+        return createValueNode(value_mapper, left().lazy_map(transformer), right().lazy_map(transformer));
+    }
+
+    template<typename Functor>
+    Tree<T> filter(Functor predicate) {
+        std::queue<Tree<T>> to_append;
+        return filter_helper(predicate, true, to_append);
+    }
+
+
+
+
+    template<typename Functor>
+    T accumulate(Functor operation, T a, Order traversal) {
         auto res = a;
         accumulate_helper(operation, res, traversal);
         return res;
     }
 
-    void accumulate_helper(std::function<T(T,T)> operation, T& a, Order traversal) const
-    {
+
+
+    template<typename Functor>
+    void apply(Functor operation, Order traversal) {
         if (empty()) {
             return;
         }
-        auto nodeFun = [&]{ a = operation(a, value()); };
-        auto leftFun = [&]{ left().accumulate_helper(operation, a, traversal); };
-        auto rightFun = [&]{ right().accumulate_helper(operation, a, traversal); };
-        traversal(nodeFun,leftFun,rightFun);
+        auto nodeFun = [&] { operation(value()); };
+        auto leftFun = [&] { left().apply(operation, traversal); };
+        auto rightFun = [&] { right().apply(operation, traversal); };
+        traversal(nodeFun, leftFun, rightFun);
     }
 
-    void apply(std::function<void(T)> operation, Order traversal) const
-    {
-        if (empty()) {
-            return;
-        }
-        auto nodeFun = [&]{operation(value());};
-        auto leftFun = [&]{left().apply(operation, traversal);};
-        auto rightFun = [&]{right().apply(operation, traversal);};
-        traversal(nodeFun,leftFun,rightFun);
-    }
-
-    size_t height() const {
-        auto fun = [](T v, size_t l, size_t r)->size_t{return std::max(l,r)+1;};
+    size_t height() {
+        auto fun = [](T v, size_t l, size_t r) -> size_t { return std::max(l, r) + 1; };
         return fold<size_t>(fun, 0);
     }
 
-    size_t size() const {
-        auto fun = [](T v, size_t l, size_t r)->size_t{return l+r+1;};
+    size_t size() {
+        auto fun = [](T v, size_t l, size_t r) -> size_t { return l + r + 1; };
         return fold<size_t>(fun, 0);
     }
 
-    bool is_bst() const {
-        using resType = std::tuple<bool,T,T>;
-        auto fun = [](T v, resType l, resType r)->resType
-        {
+    bool is_bst() {
+        using resType = std::tuple<bool, T, T>;
+        auto fun = [](T v, resType l, resType r) -> resType {
             bool l_is_bst = std::get<0>(l);
             auto l_min = std::get<1>(l);
             auto l_max = std::get<2>(l);
-            auto  r_is_bst = std::get<0>(r);
+            auto r_is_bst = std::get<0>(r);
             auto r_min = std::get<1>(r);
             auto r_max = std::get<2>(r);
-            auto is_bst =  l_is_bst && r_is_bst && l_max <= v && v <= r_min;
+            auto is_bst = l_is_bst && r_is_bst && l_max <= v && v <= r_min;
             return std::make_tuple(is_bst, l_min, r_max);
         };
-        auto result = fold<resType>(fun, std::make_tuple(true,std::numeric_limits<T>::max(),std::numeric_limits<T>::min()));
+        auto result = fold<resType>(fun, std::make_tuple(true, std::numeric_limits<T>::max(),
+                                                         std::numeric_limits<T>::min()));
         return std::get<1>(result);
+    }
+
+    bool print(Order traversal = inorder) {
+        apply([](int e) { std::cout << e << " "; }, traversal);
+        std::cout << std::endl;
     }
 };
 
 template<typename T>
-Order Tree<T>::inorder = [](Action node, Action left, Action right)->void{
-    left(); node(); right();
+Order Tree<T>::inorder = [](Action node, Action left, Action right) -> void {
+    left();
+    node();
+    right();
 };
 
 template<typename T>
-Order Tree<T>::preorder = [](Action node, Action left, Action right)->void{
-    node(); left(); right();
+Order Tree<T>::preorder = [](Action node, Action left, Action right) -> void {
+    node();
+    left();
+    right();
 };
 
 template<typename T>
-Order Tree<T>::postorder = [](Action node, Action left, Action right)->void{
-    node(); right(); left();
+Order Tree<T>::postorder = [](Action node, Action left, Action right) -> void {
+    left();
+    right();
+    node();
+};
+
+template<typename T>
+struct Tree<T>:: Node {
+    NodePtr m_left;
+    NodePtr m_right;
+
+    Node(T value, NodePtr left, NodePtr right) :
+            m_value(value),
+            m_left(std::move(left)),
+            m_right(std::move(right)) {}
+
+    Node(std::function<T()> value_creator, NodePtr left, NodePtr right) :
+            m_value_creator(value_creator),
+            m_left(std::move(left)),
+            m_right(std::move(right)) {}
+
+    Node() : m_left{}, m_right{} {}
+
+    Node(T value) : m_value(value) {}
+
+    T get_value() {
+        if (m_value_creator != nullptr && !m_value_created) {
+            m_value = m_value_creator();
+            m_value_created = true;
+        }
+        return m_value;
+    }
+private:
+    bool m_value_created = false;
+    std::function<T()> m_value_creator;
+    T m_value;
 };
 
 #endif /* TREE_H */
